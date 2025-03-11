cb759969d7559816a4dc722b45a9ecac231327d9
agruezo <agruezo@gmail.com>
2025-03-11 14:31:13 -0400

    Fix redirect function

--- backend/modules/domain_cloudfront/domain_config.tf
+++ backend/modules/domain_cloudfront/domain_config.tf
@@ -76,11 +76,13 @@ resource "aws_cloudfront_function" "redirect_html" {
             // If URL ends with .html, redirect to clean version
             if (request.uri.endsWith(".html")) {
                 var cleanUri = request.uri.replace(/\\.html$/, "");
+                var fullUrl = "https://" + request.headers.host.value + cleanUri;
+
                 return {
                     statusCode: 301,
                     statusDescription: "Moved Permanently",
                     headers: {
-                        location: { value: cleanUri },
+                        location: { value: fullUrl },
                         "cache-control": { value: "no-store" },
                         "content-type": { value: "text/html; charset=UTF-8" }
                     }
