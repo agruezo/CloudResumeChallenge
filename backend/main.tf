35a82e8744ff0fc27d6608e52bcc795b23be3b9f
agruezo <agruezo@gmail.com>
2025-03-11 06:17:46 -0400

    Update tf files to remove html from url

--- backend/main.tf
+++ backend/main.tf
@@ -21,4 +21,8 @@ module "dynamodb" {
 module "domain_cloudfront" {
   source = "./modules/domain_cloudfront"
   lambda_rewrite_function_arn = module.lambda_function.rewrite_urls_arn
+
+  depends_on = [
+    module.lambda_function
+  ]
 }
